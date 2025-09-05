40747c424411fce6d4622c1ace88ed8ff3929c4a
Emmanuelf21 <emmanuelfranco2001@gmail.com>
2025-09-05 18:58:37 -0300

    comentario

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import './index.css'
 import App from './App.jsx'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-// import Auth from './pages/Auth.jsx'
+import Auth from './pages/Auth.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<App/>}/>
-      {/* <Route path='/auth' element={<Auth/>}/> */}
+      <Route path='/auth' element={<Auth/>}/>
     </Routes>
     </BrowserRouter>
   </StrictMode>,
